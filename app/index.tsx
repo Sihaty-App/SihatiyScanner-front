bbd994a1f115160c93a440be0f1685d5a5132493
OSMaben <freegiftcardishere@gmail.com>
2025-11-30 18:41:44 +0100

    this  is the init of the project

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, Image, ImageBackground } from "react-native";
+import React from "react";
+
+
+import beachImage from "@/assets/meditation-images/beach.webp";
+
+const App = () => {
+
+    return (
+        <View className="flex-1">
+            <Text>hello</Text>
+        </View>
+    );
+};
+
+export default App;
